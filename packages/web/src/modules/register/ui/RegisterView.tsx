d2c23882717391055e17a1cfd056e5ab99c1d0b9
camilo17 <cami172013@gmail.com>
2018-06-17 19:49:19 -0500

    Made use of yup and it's integration with formik to validate Inputs as they are changed and output errors when inputs don't match desired schemas

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { withFormik, FormikErrors, FormikProps } from "formik";
 import { Form, Icon, Input, Button } from "antd";
+import * as yup from "yup";
 const FormItem = Form.Item;
 
 interface FormValues {
@@ -11,9 +12,36 @@ interface Props {
   submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
 }
 
+export const emailNotLongEnough = "email must be at least 3 characters";
+export const passwordNotLongEnough = "password must be at least 3 characters";
+export const invalidEmail = "email must be a valid email";
+
+const registerPasswordValidation = yup
+  .string()
+  .min(3, passwordNotLongEnough)
+  .max(255)
+  .required();
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required(),
+  password: registerPasswordValidation
+});
+
 class C extends React.Component<FormikProps<FormValues> & Props> {
   render() {
-    const { handleChange, values, handleBlur, handleSubmit } = this.props;
+    const {
+      handleChange,
+      values,
+      handleBlur,
+      handleSubmit,
+      touched,
+      errors
+    } = this.props;
 
     return (
       <div
@@ -32,7 +60,10 @@ class C extends React.Component<FormikProps<FormValues> & Props> {
           className="login-form"
           onSubmit={handleSubmit}
         >
-          <FormItem>
+          <FormItem
+            validateStatus={touched.email && errors.email ? "error" : "success"}
+            help={touched.email && errors.email ? errors.email : ""}
+          >
             <Input
               name="email"
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
@@ -42,7 +73,12 @@ class C extends React.Component<FormikProps<FormValues> & Props> {
               onBlur={handleBlur}
             />
           </FormItem>
-          <FormItem>
+          <FormItem
+            help={touched.password && errors.password ? errors.password : ""}
+            validateStatus={
+              touched.password && errors.password ? "error" : "success"
+            }
+          >
             <Input
               name="password"
               prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
@@ -77,6 +113,7 @@ class C extends React.Component<FormikProps<FormValues> & Props> {
 }
 
 export const RegisterView = withFormik<Props, FormValues>({
+  validationSchema,
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { setErrors, props, setSubmitting }) => {
     const errors = await props.submit(values);
