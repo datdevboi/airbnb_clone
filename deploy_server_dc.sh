2ecf7b257b4f9df5208feb684adbcb0bb5d692cd
camilo17 <cami172013@gmail.com>
2018-06-28 19:22:40 -0500

    changed bash script for heroku

--- deploy_server_dc.sh
+++ deploy_server_dc.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 yarn build:server
-docker build -t datdevboi/airbnbclone:latest .
-docker push datdevboi/airbnbclone:latest
-ssh root@67.207.84.21 "docker pull datdevboi/airbnbclone:latest && docker tag datdevboi/airbnbclone:latest dokku/airbnbclone:latest && dokku tags:deploy airbnbclone latest"
+heroku container:push --app=nameless-citadel-37358 web
+heroku container:release --app=nameless-citadel-37358 web
+# docker build -t datdevboi/airbnbclone:latest .
+# docker push datdevboi/airbnbclone:latest
+# ssh root@67.207.84.21 "docker pull datdevboi/airbnbclone:latest && docker tag datdevboi/airbnbclone:latest dokku/airbnbclone:latest && dokku tags:deploy airbnbclone latest"
