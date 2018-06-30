661040fbd64469fc8b66c4f3c18d073599793fd0
camilo17 <cami172013@gmail.com>
2018-06-30 14:28:08 -0500

    created a reusable input field

---
+++ packages/app/src/modules/shared/InputField.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { FieldProps } from "formik";
+import { Input } from "react-native-elements";
+
+const errStyle = {
+  color: "red"
+};
+export class InputField extends React.Component<
+  FieldProps<any> & { name: string }
+> {
+  onChangeText = (text: string) => {
+    this.props.form.setFieldValue(this.props.name, text);
+  };
+  render() {
+    const {
+      field, // { name, value, onChange, onBlur }
+      form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+      ...props
+    } = this.props;
+    const errorMsg = touched[field.name] && errors[field.name];
+
+    return (
+      <Input
+        {...props}
+        errorStyle={errStyle}
+        errorMessage="ENTER A VALID ERROR HERE"
+        onChangeText={this.onChangeText}
+        value={field.value}
+      />
+    );
+  }
+}
