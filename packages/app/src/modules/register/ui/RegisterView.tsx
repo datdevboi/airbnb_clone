a26b3ddcb4b4bfaa718748ff37acec3b7b4977c6
camilo17 <cami172013@gmail.com>
2018-06-30 14:28:26 -0500

    similar to our web we used formik and validUserSchema

---
+++ packages/app/src/modules/register/ui/RegisterView.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { withFormik, FormikErrors, FormikProps, Field } from "formik";
+import { View } from "react-native";
+import { InputField } from "../../shared/InputField";
+import { validUserSchema } from "@airbnbclone/common";
+
+import { Button } from "react-native-elements";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends React.Component<FormikProps<FormValues> & Props> {
+  render() {
+    const { submit, handleSubmit } = this.props;
+
+    return (
+      <View
+        style={{
+          display: "flex",
+          position: "relative",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Field name="email" placeholder="Email" component={InputField as any} />
+
+        <Field
+          name="password"
+          secureTextEntry={true}
+          placeholder="Password"
+          component={InputField as any}
+        />
+
+        <Button title="submit" onPress={handleSubmit as any} />
+      </View>
+    );
+  }
+}
+
+export const RegisterView = withFormik<Props, FormValues>({
+  validationSchema: validUserSchema,
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { setErrors, props, setSubmitting }) => {
+    const errors = await props.submit(values);
+    if (errors) {
+      setErrors(errors);
+    }
+  }
+})(C);
