39d92e5363042318672e02c5b124890ac9270173
camilo17 <cami172013@gmail.com>
2018-07-03 16:14:26 -0500

    created a LoginView

---
+++ packages/web/src/modules/login/ui/LoginView.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { withFormik, FormikErrors, FormikProps, Field } from "formik";
+import { Form, Icon, Button } from "antd";
+import { Link } from "react-router-dom";
+import { validUserSchema } from "@airbnbclone/common";
+import { InputField } from "../../shared/InputField";
+const FormItem = Form.Item;
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends React.Component<FormikProps<FormValues> & Props> {
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          position: "relative",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Form
+          style={{ width: 500 }}
+          className="login-form"
+          onSubmit={handleSubmit}
+        >
+          <Field
+            name="email"
+            placeholder="Email"
+            prefix={
+              <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} /> as any
+            }
+            component={InputField}
+          />
+
+          <Field
+            name="password"
+            type="password"
+            placeholder="Password"
+            prefix={
+              <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} /> as any
+            }
+            component={InputField}
+          />
+
+          <FormItem>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Login
+            </Button>
+          </FormItem>
+          <FormItem>
+            Or <Link to="/register">Register</Link>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export const LoginView = withFormik<Props, FormValues>({
+  validationSchema: validUserSchema,
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { setErrors, props, setSubmitting }) => {
+    const errors = await props.submit(values);
+    if (errors) {
+      setErrors(errors);
+    }
+  }
+})(C);
