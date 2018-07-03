ec988ea935e0c68a8e21d1a5dacc049a2ef2496f
camilo17 <cami172013@gmail.com>
2018-07-03 16:15:16 -0500

    added new route for login connector

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { RegisterConnector } from "../modules/register/RegisterConnector";
+import { LoginConnector } from "../modules/login/LoginConnector";
 
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact={true} path="/" component={LoginConnector} />
       <Route exact={true} path="/register" component={RegisterConnector} />
     </Switch>
   </BrowserRouter>
