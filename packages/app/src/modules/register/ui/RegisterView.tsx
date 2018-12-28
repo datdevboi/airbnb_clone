6f1693edd57b9b629c639c5a552220fddeab944f
datdevboi <cami172013@gmail.com>
2018-12-27 18:35:59 -0600

    Updated the Ui of our registerView

--- packages/app/src/modules/register/ui/RegisterView.tsx
+++ packages/app/src/modules/register/ui/RegisterView.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { withFormik, FormikErrors, FormikProps, Field } from "formik";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { InputField } from "../../shared/InputField";
 import { validUserSchema } from "@airbnbclone/common";
 
-import { Button } from "react-native-elements";
+import { Button, Card } from "react-native-elements";
 
 interface FormValues {
   email: string;
@@ -23,22 +23,33 @@ class C extends React.Component<FormikProps<FormValues> & Props> {
         style={{
           flex: 1,
           display: "flex",
-
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center"
+          justifyContent: "center"
         }}
       >
-        <Field name="email" placeholder="Email" component={InputField as any} />
+        <Card>
+          <Text style={{ fontSize: 30, marginBottom: 10 }}>Register</Text>
+          <Field
+            name="email"
+            placeholder="Email"
+            component={InputField as any}
+            containerStyle={{ width: "100%" }}
+            autoCapitalize="none"
+          />
 
-        <Field
-          name="password"
-          secureTextEntry={true}
-          placeholder="Password"
-          component={InputField as any}
-        />
+          <Field
+            name="password"
+            secureTextEntry={true}
+            placeholder="Password"
+            component={InputField as any}
+            containerStyle={{ width: "100%" }}
+          />
 
-        <Button title="Register" onPress={handleSubmit as any} />
+          <Button
+            style={{ marginTop: 20 }}
+            title="Register"
+            onPress={handleSubmit as any}
+          />
+        </Card>
       </View>
     );
   }
