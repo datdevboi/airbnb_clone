10f45f608a2c29eddec486a0f3de0b601dc76f50
datdevboi <cami172013@gmail.com>
2018-12-28 16:52:23 -0600

    created ForgotPasswordController and exported it in our index.ts

---
+++ packages/controller/src/modules/ForgotPasswordController/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { graphql, ChildMutateProps } from "react-apollo";
+import gql from "graphql-tag";
+
+interface IProps {
+  children: (
+    data: { submit: (values: any) => Promise<null> }
+  ) => JSX.Element | null;
+}
+
+class FP extends React.PureComponent<ChildMutateProps<IProps, any, any>> {
+  submit = async (values: any) => {
+    await this.props.mutate({
+      variables: values
+    });
+
+    return null;
+  };
+  render() {
+    return this.props.children({
+      submit: this.submit
+    });
+  }
+}
+
+const forgotPasswordMutation = gql`
+  mutation SendForgotPasswordMutation($email: String!) {
+    sendForgotPasswordEmail(email: $email) 
+    }
+  }
+`;
+
+export const ForgotPasswordControler = graphql(forgotPasswordMutation)(FP);
