1d438ae77c0952e536eee94ae87e9eeeab50df0f
datdevboi <cami172013@gmail.com>
2018-12-28 16:55:31 -0600

    updated our host variable

--- packages/app/src/apollo.ts
+++ packages/app/src/apollo.ts
@@ -3,9 +3,10 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { Platform } from "react-native";
 
-const host =
-  Platform.OS === "ios" ? "http://localhost:4000" : "http://10.0.2.2:4000";
+// const host =
+//   Platform.OS === "ios" ? "http://localhost:4000" : "http://10.0.2.2:4000";
 
+const host = "http://192.168.0.114:3000/";
 export const client = new ApolloClient({
   link: new HttpLink({
     uri: host
