eae8a374bfd7ea765064e6ac45f497aff4f209d3
datdevboi <cami172013@gmail.com>
2018-12-29 18:29:27 -0600

    Made use of our ChangePasswordController

--- packages/web/src/modules/changePassword/ChangePasswordConnector.tsx
+++ packages/web/src/modules/changePassword/ChangePasswordConnector.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
 import { ChangePasswordView } from "./ui/ChangePasswordView";
 import { ChangePasswordController } from "@airbnbclone/controller";
+import { RouteComponentProps } from "react-router-dom";
 
-export class ChangePasswordConnector extends React.Component<any> {
+export class ChangePasswordConnector extends React.Component<
+  RouteComponentProps<{
+    key: string;
+  }>
+> {
   render() {
-    const { id } = this.props.match.params;
+    const { key } = this.props.match.params;
 
     return (
       <ChangePasswordController>
         {({ submit }: { submit: any }) => (
-          <ChangePasswordView id={id} submit={submit} />
+          <ChangePasswordView
+            // tslint:disable-next-line:jsx-no-lambda
+            submit={({ newPassword }) =>
+              submit({
+                newPassword,
+                key
+              })
+            }
+          />
         )}
       </ChangePasswordController>
     );
