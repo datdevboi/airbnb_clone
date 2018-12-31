51e05739e37c8b33082a4a9e3e27eeded0fd8800
datdevboi <cami172013@gmail.com>
2018-12-30 18:27:15 -0600

    updated RegisterConnector, passed onFinish prop to RegisterView

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import { RegisterView } from "./ui/RegisterView";
 import { RegisterController } from "@airbnbclone/controller";
+import { RouteComponentProps } from "react-router-dom";
 
-export class RegisterConnector extends React.Component {
+export class RegisterConnector extends React.Component<
+  RouteComponentProps<{}>
+> {
+  onFinish = () => {
+    this.props.history.push("/m/confirm-email", {
+      message: "check your email to confirm your account"
+    });
+  };
   render() {
     return (
       <RegisterController>
-        {({ submit }: { submit: any }) => <RegisterView submit={submit} />}
+        {({ submit }: { submit: any }) => (
+          <RegisterView submit={submit} onFinish={this.onFinish} />
+        )}
       </RegisterController>
     );
   }
