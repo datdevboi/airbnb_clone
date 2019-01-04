0e09a8b525b507910639be9ef90ddd2ca78f01c4
datdevboi <cami172013@gmail.com>
2019-01-03 18:53:59 -0600

    made sure user was signed before creating listing

--- packages/server/src/modules/listing/create/resolvers.ts
+++ packages/server/src/modules/listing/create/resolvers.ts
@@ -4,6 +4,10 @@ import { Listing } from "../../../entity/Listing";
 export const resolvers: ResolverMap = {
   Mutation: {
     createListing: async (_, { input }, { session }, __) => {
+      if (!session.userId) {
+        // user is not logged in
+        throw new Error("not autheticated");
+      }
       await Listing.create({
         ...input,
         pictureUrl: "",
