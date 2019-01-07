72e70bc75f757b442906149a02f39d17c58ffa8f
datdevboi <cami172013@gmail.com>
2019-01-06 18:02:02 -0600

    updated InputField to take a label prop to use in FormItem

--- packages/web/src/modules/shared/InputField.tsx
+++ packages/web/src/modules/shared/InputField.tsx
@@ -5,16 +5,21 @@ import { Form, Input } from "antd";
 const FormItem = Form.Item;
 
 export const InputField: React.SFC<
-  FieldProps<any> & { prefix: React.ReactNode }
+  FieldProps<any> & { prefix: React.ReactNode; label?: string }
 > = ({
   field, // { name, value, onChange, onBlur }
   form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  label,
   ...props
 }) => {
   const errorMsg = touched[field.name] && errors[field.name];
 
   return (
-    <FormItem validateStatus={errorMsg ? "error" : "success"} help={errorMsg}>
+    <FormItem
+      label={label}
+      validateStatus={errorMsg ? "error" : "success"}
+      help={errorMsg}
+    >
       <Input {...field} {...props} />
     </FormItem>
   );
