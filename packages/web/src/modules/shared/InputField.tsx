e8b81319b81dbf63ec1adb3af49f62cf277b34d1
datdevboi <cami172013@gmail.com>
2019-01-11 18:21:19 -0600

    updated InputField to handle InputNumber

--- packages/web/src/modules/shared/InputField.tsx
+++ packages/web/src/modules/shared/InputField.tsx
@@ -1,18 +1,25 @@
 import * as React from "react";
 import { FieldProps } from "formik";
-import { Form, Input } from "antd";
+import { Form, Input, InputNumber } from "antd";
 
 const FormItem = Form.Item;
 
 export const InputField: React.SFC<
-  FieldProps<any> & { prefix: React.ReactNode; label?: string }
+  FieldProps<any> & {
+    prefix: React.ReactNode;
+    label?: string;
+    useNumberComponent?: boolean;
+  }
 > = ({
-  field, // { name, value, onChange, onBlur }
-  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  field: { onChange, ...field }, // { name, value, onChange, onBlur }
+  form: { touched, errors, setFieldValue }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
   label,
+  useNumberComponent = false,
   ...props
 }) => {
   const errorMsg = touched[field.name] && errors[field.name];
+
+  const Comp: any = useNumberComponent ? InputNumber : Input;
 
   return (
     <FormItem
@@ -20,7 +27,17 @@ export const InputField: React.SFC<
       validateStatus={errorMsg ? "error" : "success"}
       help={errorMsg}
     >
-      <Input {...field} {...props} />
+      <Comp
+        {...field}
+        {...props}
+        onChange={
+          useNumberComponent
+            ? (value: any) => {
+                setFieldValue(field.name, value);
+              }
+            : onChange
+        }
+      />
     </FormItem>
   );
 };
