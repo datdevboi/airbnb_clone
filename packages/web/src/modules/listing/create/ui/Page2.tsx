4836729dec5a38c8b22ae8fe7f9294346860d6d4
datdevboi <cami172013@gmail.com>
2019-01-11 18:21:47 -0600

    made use of our useNumberComponentProp

--- packages/web/src/modules/listing/create/ui/Page2.tsx
+++ packages/web/src/modules/listing/create/ui/Page2.tsx
@@ -1,19 +1,31 @@
 import * as React from "react";
 import { Field } from "formik";
-
-import { InputNumber } from "antd";
+import { InputField } from "../../../../modules/shared/InputField";
 
 export const Page2 = () => (
   <>
-    <Field label="Price" name="price" placeholder="0" component={InputNumber} />
+    <Field
+      label="Price"
+      name="price"
+      placeholder="0"
+      component={InputField}
+      useNumberComponent={true}
+    />
 
-    <Field label="Beds" name="beds" placeholder="0" component={InputNumber} />
+    <Field
+      label="Beds"
+      name="beds"
+      placeholder="0"
+      component={InputField}
+      useNumberComponent={true}
+    />
 
     <Field
       label="Guests"
       name="guests"
       placeholder="0"
-      component={InputNumber}
+      component={InputField}
+      useNumberComponent={true}
     />
   </>
 );
