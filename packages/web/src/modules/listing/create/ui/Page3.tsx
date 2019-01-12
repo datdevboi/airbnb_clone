167d186a8f0717967b7e09a7872e4216513a0650
datdevboi <cami172013@gmail.com>
2019-01-11 18:36:51 -0600

    made use of TagField

--- packages/web/src/modules/listing/create/ui/Page3.tsx
+++ packages/web/src/modules/listing/create/ui/Page3.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Field } from "formik";
 import { InputField } from "../../../../modules/shared/InputField";
+import { TagField } from "../../../../modules/shared/TagField";
 
 export const Page3 = () => (
   <>
@@ -23,7 +24,7 @@ export const Page3 = () => (
     <Field
       label="Amenities"
       name="amenities"
-      component={InputField}
+      component={TagField}
       useNumberComponent={true}
     />
   </>
