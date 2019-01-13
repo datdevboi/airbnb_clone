67c4cdfa9da513841ac12a44140bb97416787a99
datdevboi <cami172013@gmail.com>
2019-01-12 18:46:53 -0600

    exported CreateListing module

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -2,6 +2,7 @@ export * from "./modules/RegisterController";
 export * from "./modules/LoginController";
 export * from "./modules/ForgotPasswordController";
 export * from "./modules/ChangePasswordController";
+export * from "./modules/CreateListing";
 export * from "./modules/auth";
 export * from "./schemaTypes";
 export * from "./modules/types/NormalizeErrorMap";
